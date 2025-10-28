20e662f9436dcc755926f57a8b609dbebf5265ae
jetrx32 <jetrx32@gmail.com>
2025-10-28 12:34:43 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for ID in $(seq 1 30); do
+  cd aztec-sequencer-ipnode-${ID}
+  ls -la
+  sleep 600
+  docker compose down 
+  sed -i 's|aztecprotocol/aztec:[^"]*|aztecprotocol/aztec:2.0.4|' docker-compose.yml && \
+  echo "GOVERNANCE_PROPOSER_PAYLOAD_ADDRESS=0xDCd9DdeAbEF70108cE02576df1eB333c4244C666" >> .env
+  docker compose up -d
+  cd
+  sleep $(( RANDOM % 601 + 600 ))
+done
